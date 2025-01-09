213833b6490cb17345b0071e116372ddd0f15133
omer_yavas <omer.yavas@solmaz.com>
2025-01-09 10:43:39 +0300

    By adding Inter font to the <body> element, the font will be applied throughout your application.

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+// You'll use this file to keep the fonts that will be used throughout your application.
+import { Inter } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
