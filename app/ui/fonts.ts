a9e25ab752529fb8b4801a61017d49c6d7071ebe
omer_yavas <omer.yavas@solmaz.com>
2025-01-09 15:42:54 +0300

    uygulamaya font  ve ana sayfa için resimler eklendi

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,9 @@
 // You'll use this file to keep the fonts that will be used throughout your application.
-import { Inter } from "next/font/google";
+import { Inter, Lusitana } from "next/font/google";
 
 export const inter = Inter({ subsets: ["latin"] });
+
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
